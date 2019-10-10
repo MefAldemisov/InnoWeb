a9f746b863a897d08f8422bd4f54d790b25e97f9
MefAldemisov <alisha-omg@yandex.ru>
2019-10-10 23:54:37 +0300

    4.4 - 1 - Films output modified

--- js/films.js
+++ js/films.js
@@ -1,47 +1,30 @@
 // Table
 
-// ganres
 const ganres = ['фантастика', 'боевик', 'приключения', 'фэнтези', 'драмма', 'коммедия', 'мультфильм']
 
-// timings
-let start_film = [];
-start_film[0] = '10:00';
-start_film[1] = '12:00';
-start_film[2] = '14:00';
-start_film[3] = '16:00';
+let films = [
+    ['10:00', 'Человек-паук', '0/1/2'],
+    ['12:00', 'Собачья жизнь 2', '3/4/5'],
+    ['14:00', 'История игрушек 4', '6/3/5'],
+    ['16:00', 'Люди в чёрном: Интернэшнл', '0/1/5']
+]
 
-// film-names
-let name_film = [];
-name_film[0] = 'Человек-паук'
-name_film[1] = 'Собачья жизнь 2'
-name_film[2] = 'История игрушек 4'
-name_film[3] = 'Люди в чёрном: Интернэшнл'
-
-//  conacatenating
-let ganre_film = []; 
-ganre_film[0] = ganres[0] + ', ' + ganres[1] +', ' + ganres[2]
-ganre_film[1] = ganres[3] + ', ' + ganres[4] +', ' + ganres[5]
-ganre_film[2] = ganres[6] + ', ' + ganres[3] +', ' + ganres[5]
-ganre_film[3] = ganres[0] + ', ' + ganres[1] +', ' + ganres[5]
-
-// output (get info from HTML)
-for (var i=0; i<4; i++){
-    console.log('element-'+i+' name',document.getElementById('name_film_'+(i+1)))
-
+for (var i=0; i<films.length; i++) {
+    // get indexes 
+    var ganres_indexes  = films[i][2].split('/');
+    var film_ganre = ''
+    for (var j=0; j < ganres_indexes.length; j++){
+        film_ganre += ganres[ganres_indexes[j]]
+        if (j<ganres_indexes.length-1){
+            film_ganre += ', '
+        }
+    }
+    // get the elements
     var element_start_film = document.getElementById('start_film_'+(i+1))
     var element_name_film = document.getElementById('name_film_'+(i+1))
     var element_ganre_film = document.getElementById('ganre_film_'+(i+1))
-
-    console.log('element-'+i+' start', element_start_film)
-
-    // content editing
-
-    element_start_film.innerHTML = start_film[i]
-    element_name_film.innerHTML = name_film[i]
-    element_ganre_film.innerHTML = ganre_film[i]
-
-    console.log('element-'+i+' new start', element_start_film)
-
-    // output is always the same
+    // add values
+    element_start_film.innerHTML = films[i][0]
+    element_name_film.innerHTML = films[i][1]
+    element_ganre_film.innerHTML = film_ganre
 }
-console.log('finish')
